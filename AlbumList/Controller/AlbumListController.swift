4ca02183bad2d4d5fb5ee4ba4e2e3e6ebb46c239
Abin <abin.baby@riskmethods.net>
2020-10-26 12:46:31 +0100

    Adds table view

--- AlbumList/Controller/AlbumListController.swift
+++ AlbumList/Controller/AlbumListController.swift
@@ -9,12 +9,21 @@ import UIKit
 
 class AlbumListController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    let tableView: UITableView = UITableView()
 
-        // Do any additional setup after loading the view.
-    }
-    
+      override func loadView() {
+        super.loadView()
+        setupTableView()
+      }
+
+      func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+      }
 
     /*
     // MARK: - Navigation
