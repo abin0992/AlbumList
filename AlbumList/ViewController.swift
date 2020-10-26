785d03b6a2b19ed0859ba9fc1018f9a03902229f
Abin <abin.baby@riskmethods.net>
2020-10-26 12:30:34 +0100

    Initial Commit

---
+++ AlbumList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumList
+//
+//  Created by Abin Baby on 26/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
