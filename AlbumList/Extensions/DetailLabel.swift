545872a3ecef0b2fd4507fabc615a58bfd75185c
Abin <abin.baby@riskmethods.net>
2020-10-28 09:42:53 +0100

    Adds custom label

--- AlbumList/Extensions/DetailLabel.swift
+++ AlbumList/Extensions/DetailLabel.swift
@@ -5,4 +5,23 @@
 //  Created by Abin Baby on 27/10/20.
 //
 
-import Foundation
+import UIKit
+
+class DetailLabel: UILabel {
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLabel()
+    }
+
+    func setupLabel() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.contentMode = .center
+        self.numberOfLines = 0
+        self.lineBreakMode = .byWordWrapping
+    }
+}
