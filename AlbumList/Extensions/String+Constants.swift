0aedb928895668e504f76529fdb62a31b2d57006
Abin <abin.baby@riskmethods.net>
2020-10-28 17:07:31 +0100

    refactoring

--- AlbumList/Extensions/String+Constants.swift
+++ AlbumList/Extensions/String+Constants.swift
@@ -9,6 +9,6 @@ import Foundation
 
 extension String {
     struct Constants {
-        static let albumsListUrl: String = "https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/100/non-explicit.json"
+        static let albumsListUrl: String = "https://rss.itunes.apple.com/api/v1/us/apple-music/coming-soon/all/100/explicit.json"
     }
 }
